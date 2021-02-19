31734344dcbebda3e7cbfa099041fa912894cb6c
Andrei Cristian Busila <andreibusila9819@gmail.com>
2021-02-19 10:41:32 +0200

    deleted "nextInt" in Scanner

--- src/main/java/org/example/domain/Ship.java
+++ src/main/java/org/example/domain/Ship.java
@@ -127,5 +127,4 @@ public class Ship {
         return test;
     }
 
-
 }
--- src/main/java/org/example/utils/ScannerUtils.java
+++ src/main/java/org/example/utils/ScannerUtils.java
@@ -5,13 +5,6 @@ import java.util.Scanner;
 public class ScannerUtils {
     static final Scanner SCANNER = new Scanner(System.in);
 
-    public static int nextIntAndMoveToNextLine() {
-        int integer = SCANNER.nextInt();
-        SCANNER.nextLine();
-        return integer;
-    }
-
-
     public static String nextLine() {
         return SCANNER.nextLine();
     }
